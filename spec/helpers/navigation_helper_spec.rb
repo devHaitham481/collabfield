a121d124c00b62a4149c8b687aa2681efd756474
devHaitham481 <devHaitham481@gmail.com>
2021-04-24 05:58:06 +0300

    Add specs to NavigationHelper's collapsible_links_partial_path method -- fixed failures

--- spec/helpers/navigation_helper_spec.rb
+++ spec/helpers/navigation_helper_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe NavigationHelper, type: :helper do
 
     context '#collapsible_links_partial_path' do 
       it 'returns signed_in_links_partials path' do 
-        expect(helper.collapsible_links_partial_path).to
+        expect(helper.collapsible_links_partial_path).to (
         eq('layouts/navigation/collapsible_elements/signed_in_links')
-
+        )
       end
     end
   end
@@ -22,8 +22,9 @@ RSpec.describe NavigationHelper, type: :helper do
 
     context '#collapsible_links_partial_path' do 
       it "returns non_signed_in_links_partial" do 
-        expect(helper.collapsible_links_partial_path).to
-        eq('layouts/navigation/collapsible_elements/non_signed_in_links')
+        expect(helper.collapsible_links_partial_path).to (
+          eq('layouts/navigation/collapsible_elements/non_signed_in_links')
+        )
       end
     end
   end
